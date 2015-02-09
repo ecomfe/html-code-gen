5a86f6fae89954f4b57545c8004f554b451cbaf7
nighca <nighca@live.cn>
2015-02-09 14:29:18 +0800

    file for browser updated

--- browser/html-code-gen.js
+++ browser/html-code-gen.js
@@ -1,4 +1,16 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.htmlCodeGen=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*
+ * gen code with given dom(-like) object
+ * nighca ( nighca@live.cn )
+ */
+
+var print = require('./lib/print');
+
+module.exports = {
+    print: print
+};
+
+},{"./lib/print":3}],2:[function(require,module,exports){
 /**
  * @file print method for element
  * @author nighca<nighca@live.cn>
@@ -145,7 +157,7 @@ var printElementNode = function(node, opt){
 };
 
 module.exports = printElementNode;
-},{"./print":2,"./spec":3,"./util":4}],2:[function(require,module,exports){
+},{"./print":3,"./spec":4,"./util":5}],3:[function(require,module,exports){
 /**
  * @file print methods for different kinds of node
  * @author nighca<nighca@live.cn>
@@ -270,7 +282,7 @@ var print = function(node, opt){
 };
 
 module.exports = print;
-},{"./print-element":1,"./spec":3,"./util":4}],3:[function(require,module,exports){
+},{"./print-element":2,"./spec":4,"./util":5}],4:[function(require,module,exports){
 /**
  * @file some spec info
  * @author nighca<nighca@live.cn>
@@ -312,7 +324,7 @@ module.exports = {
     tagTypeMap: tagTypeMap,
     booleanAttributes: booleanAttributes
 };
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 /**
  * @file util methods
  * @author nighca<nighca@live.cn>
@@ -358,16 +370,5 @@ module.exports = {
     indent: indent,
     isIn: isIn
 };
-},{}],"html-code-gen":[function(require,module,exports){
-/*
- * gen code with given dom(-like) object
- * nighca ( nighca@live.cn )
- */
-
-var print = require('./lib/print');
-
-module.exports = {
-    print: print
-};
-
-},{"./lib/print":2}]},{},[]);
+},{}]},{},[1])(1)
+});
